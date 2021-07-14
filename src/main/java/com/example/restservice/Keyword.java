ce72226b929f3fec7c21353dff67db5aa8254f8c
mohmadanasshb21@gmail.com <mohmadanasshb21@gmail.com>
2021-07-14 12:51:15 +0300

    Adding jsonDateFormat

--- src/main/java/com/example/restservice/Keyword.java
+++ src/main/java/com/example/restservice/Keyword.java
@@ -1,5 +1,6 @@
 package com.example.restservice;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import java.util.Date;
@@ -15,7 +16,9 @@ public class Keyword {
     int totalMentions;
     double avgPerDay;
     int numOfPosts;
+    @JsonFormat(pattern = "dd/MM/yyyy")
     Date startDate;
+    @JsonFormat(pattern = "dd/MM/yyyy")
     Date endDate;
 
     public Keyword(Set<String>websites,String word ,int totalMentions ,double avgPerDay ,int numOfPosts , Date startDate , Date endDate){
