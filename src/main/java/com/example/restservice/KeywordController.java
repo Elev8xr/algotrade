b9b80f0323e02412e869db3cf0eef099fa490b53
mohmadanasshb21@gmail.com <mohmadanasshb21@gmail.com>
2021-07-14 08:56:10 +0300

    Convert the Respone to Json , And adding the Json dependency

--- src/main/java/com/example/restservice/KeywordController.java
+++ src/main/java/com/example/restservice/KeywordController.java
@@ -1,9 +1,10 @@
 package com.example.restservice;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.springframework.web.bind.annotation.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 
@@ -17,8 +18,11 @@ public class KeywordController {
     @RequestMapping (value = "keyword", method = RequestMethod.GET)
     public String greeting(@RequestParam(value = "keyword",defaultValue = "null")String keyword ,
                             @RequestParam(value = "startDate",defaultValue = "1/1/2020")String startDate , @RequestParam(value = "endDate",defaultValue = "12/12/2020")String endDate) throws ParseException, JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
         Keyword keywordResult = manager.findKeyword(keyword, startDate, endDate);
-        return keywordResult.toString();
+        String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(keywordResult);
+        return json;
 
     }
 
